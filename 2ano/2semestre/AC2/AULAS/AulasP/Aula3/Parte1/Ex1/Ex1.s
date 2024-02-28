658edad805b2c2f66e35465af2b9f15ccf827ff1
pedromelo322 <pedromelo1316@gmail.com>
2024-02-28 10:00:53 +0000

    AC-Material Apoio e Ex1.s

---
+++ 2ano/2semestre/AC2/AULAS/AulasP/Aula3/Parte1/Ex1/Ex1.s
@@ -0,0 +1,41 @@
+        .data
+
+        .equ ADDR_BASE_HI,0xBF88 # Base address: 16 MSbits
+        .equ TRISB,0x6040 # TRISE address is 0xBF886100
+        .equ PORTB,0x6050 # PORTE address is 0xBF886110
+        .equ LATB,0x6060 # LATE address is 0xBF886120
+        .equ TRISE,0x6100 # TRISE address is 0xBF886100
+        .equ PORTE,0x6110 # PORTE address is 0xBF886110
+        .equ LATE,0x6120 # LATE address is 0xBF886120
+
+
+        .text
+        .globl main
+
+
+
+main:
+
+
+        lui $t1, ADDR_BASE_HI
+        lw $t2, TRISE($t1) # Read TRISE
+        andi $t2, $t2, 0xFFFE # RE0 -> saida
+        sw $t2, Trise($t1) # Write TRISE
+
+        lw $t2, TRISB($t1) # Read TRISB
+        ori $t2, $t2, 0x0001 # RB0 -> entrada
+        sw $t2, TRISB($t1) #write TRISB
+
+
+
+while:
+
+        lw $t2, PORTB($t1)
+        sw $t2, LATE($t1)
+
+
+        j while
+
+        jr $ra
+
+
