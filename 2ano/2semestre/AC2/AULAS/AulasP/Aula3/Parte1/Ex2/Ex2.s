09f5308092efb35f3c66446adddc31a7b943cb1a
pedromelo322 <pedromelo1316@gmail.com>
2024-02-28 10:32:54 +0000

    AC-Ex2

--- 2ano/2semestre/AC2/AULAS/AulasP/Aula3/Parte1/Ex2/Ex2.s
+++ 2ano/2semestre/AC2/AULAS/AulasP/Aula3/Parte1/Ex2/Ex2.s
@@ -1,5 +1,5 @@
-        .data        
-        
+        .data
+
         .equ ADDR_BASE_HI,0xBF88 # Base address: 16 MSbits
         .equ TRISB,0x6040 # TRISB address is 0xBF886040
         .equ PORTB,0x6050 # PORTB address is 0xBF886050
@@ -8,43 +8,42 @@
         .equ PORTE,0x6110 # PORTE address is 0xBF886110
         .equ LATE,0x6120 # LATE address is 0xBF886120
 
-
         .text
         .globl main
 
 
-
 main:
 
-
         lui $t1, ADDR_BASE_HI
-        lw $t2, TRISE($t1) # Read TRISE
-        andi $t2, $t2, 0xFFFE # RE0 -> saida
-        sw $t2, Trise($t1) # Write TRISE
+        lw $t2, TRISE($t1)
+        andi $t2, $t2, 0xFFFE #Saida
+        sw $t2, TRISE($t1)
 
-        lw $t2, TRISB($t1) # Read TRISB
-        ori $t2, $t2, 0x0001 # RB0 -> entrada
-        sw $t2, TRISB($t1) #write TRISB
+        lw $t3, TRISB($t1)
+        ori $t3, $t3, 0x0001 #Entrada
+        sw $t3, TRISB($t1)
+
 
 
 
 while:
 
-        lw $t2, PORTB($t1)
-        lw $t3, LATE($t1)
+        lw $t2, LateE($t1)  #saida
+        lw $t3, PORTB($t1)  #entrada
+        
 
+        
+        andi $t2, $t2, 0xFFFE
+        andi $t3, $t3, 0x0001 
+        
+        or $t2, $t2, $t3
 
-        andi $t2, $t2, 0x0001
-        andi $t3, $t3, 0xFFFE
-
-        or $t3, $t3, $t2
-
-
-        sw $t3, LATE($t1)
-
+        sw $t2, 0($t1)
+        
 
         j while
 
         jr $ra
+
 
 
