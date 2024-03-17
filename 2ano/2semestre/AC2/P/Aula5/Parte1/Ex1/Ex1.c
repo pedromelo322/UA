a86d46f8e8086c83bc77d9d3c9061b3848ea606f
Pedro Melo <pedromelo1316@gmail.com>
2024-03-17 14:23:01 +0000

    Teste SSE-P1 e AC2- AULA5 PARTE1-EX5.1 e 5.2 corrigidos

--- 2ano/2semestre/AC2/P/Aula5/Parte1/Ex1/Ex1.c
+++ 2ano/2semestre/AC2/P/Aula5/Parte1/Ex1/Ex1.c
@@ -4,11 +4,11 @@ void send2displays(unsigned char value){
 
     static const char disp7Scodes[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x67, 0x77, 0x7c, 0x39, 0x5e, 0x79 ,0x71};
 
-    LATD = (LATD & 0XFF9) | 0X0040;
+    LATD = (LATD & 0XFF9F) | 0X0040;
 
     LATB = (LATB & 0x80ff) | (disp7Scodes[value >> 4] << 8);
 
-    LATD = (LATD & 0XFF9) | 0X0020;
+    LATD = (LATD & 0XFF9F) | 0X0020;
 
     LATB = (LATB & 0x80ff) | (disp7Scodes[value & 0x0F] << 8);
 
