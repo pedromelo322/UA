75b52b36418880780d02150aab0f994b02c4b608
pedromelo <pedromelo1316@gmail.com>
2024-06-22 19:27:29 +0100

    ...

--- 2ano/2semestre/CTS/MICSA/Projeto/Projeto.ino
+++ 2ano/2semestre/CTS/MICSA/Projeto/Projeto.ino
@@ -1,117 +1,130 @@
+// Inclusão de bibliotecas
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT11.h>
 
+// Definição de pinos
 #define MQ2pin A0
 #define POTENTIOMETER_PIN A1
 #define SWITCH_PIN 7
-#define LED_PIN 9
 #define FAN_PIN 10
 #define BUZZER_PIN 8
 
+// Definição de limites para temperatura e gás
 #define MIN_TEMPERATURE 20
 #define MAX_TEMPERATURE 40
-#define MIN_GAS 150
-#define MAX_GAS 350
+#define MIN_GAS 300
+#define MAX_GAS 600
 
+// Inicialização de objetos
 DHT11 dht11(2);
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Initialize LCD with address 0x27, 16 columns and 2 rows
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Inicializa o LCD com endereço 0x27, 16 colunas e 2 linhas
 
+// Variáveis globais
 int forceChange = 0;
-float gasSensorValue;
-int temperatureThreshold = 300; // Default value
-int gasThreshold = 250; // Default value
-bool displayMode = true; // true for temperature/humidity, false for gas
+int gasSensorValue;
+int temperature;  
+int humidity;
+int temperatureThreshold = 300; // Valor padrão
+int gasThreshold = 500; // Valor padrão
+bool displayMode = true; // true para temperatura, false para gás
 
 void setup() {
-  lcd.init();  // Initialize the LCD
-  lcd.backlight();  // Turn on the LCD backlight
+  lcd.init(); 
+  lcd.backlight();
   Serial.begin(9600);
 
   pinMode(BUZZER_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
-  pinMode(SWITCH_PIN, INPUT); // Set as INPUT
+  pinMode(SWITCH_PIN, INPUT);
 
-  Serial.println("MQ2 warming up!");
-  delay(20000); // Warm-up time for the gas sensor
+  Serial.println("MQ2 a aquecer!");
+  delay(20000); // Tempo de aquecimento para o sensor de gás
 }
 
 void loop() {
-  int temperature = 0;
-  int humidity = 0;
+
   int result = dht11.readTemperatureHumidity(temperature, humidity);
 
   gasSensorValue = analogRead(MQ2pin);
   int potValue = analogRead(POTENTIOMETER_PIN);
   bool switchState = digitalRead(SWITCH_PIN);
 
-  // Clear the screen
-  lcd.clear();
+  Serial.print(switchState);
 
-  // Toggle display mode
-  if (forceChange == 1 || (forceChange != 2 && switchState == HIGH)) {
+  
+  if (switchState == HIGH) {
+    displayMode = !displayMode;
+  }
+
+  // Mostra os valores apropriados com base no modo de exibição
+  if (forceChange == 1 || (forceChange != 2 && displayMode == true)) {
     temperatureThreshold = map(potValue, 0, 1023, MIN_TEMPERATURE, MAX_TEMPERATURE);
     displayTemperatureHumidity(temperature, humidity);
-  } else if (forceChange == 2 || (forceChange != 1 && switchState == LOW)) {
+  } else if (forceChange == 2 || (forceChange != 1 && displayMode == false)) {
     gasThreshold = map(potValue, 0, 1023, MIN_GAS, MAX_GAS);
     displayGasValue();
   }
 
-  controlFanAndLed(temperature);
-  delay(1000); // Delay for reading and updating values
+  controlFan();
+  delay(250); // Atraso para leitura e atualização de valores
 
   printDebug(temperature, temperatureThreshold, gasSensorValue, gasThreshold);
 }
 
+// Função para mostrar temperatura no LCD
 void displayTemperatureHumidity(int temperature, int humidity) {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(temperature);
   lcd.print(" C");
   lcd.setCursor(0, 1);
-  lcd.print("Thresh: ");
+  lcd.print("Threshold: ");
   lcd.print(temperatureThreshold);
+  lcd.print(" C");
 }
 
+// Função para mostrar o valor do gás no LCD
 void displayGasValue() {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Gas: ");
   lcd.print(gasSensorValue);
   lcd.setCursor(0, 1);
-  lcd.print("Thresh: ");
+  lcd.print("threshold: ");
   lcd.print(gasThreshold);
 }
 
-void controlFanAndLed(int temperature) {
+// Função para controlar o ventilador com base na temperatura e no nível de gás
+void controlFan() {
   if (temperature >= temperatureThreshold || gasSensorValue >= gasThreshold) {
-    digitalWrite(LED_PIN, HIGH);
-    digitalWrite(FAN_PIN, LOW); // Corrected logic to turn on the fan
+    digitalWrite(FAN_PIN, LOW); 
     forceChange = (temperature >= temperatureThreshold) ? 1 : 2;
     for (int i = 0; i < 3; i++) { // Repete o som 3 vezes
-      tone(BUZZER_PIN, 1000); // Inicia o som
-      delay(200); // Duração do som
-      noTone(BUZZER_PIN); // Para o som
-      if (i < 2) { // Evita a pausa após o último som
-        delay(200); // Intervalo entre os sons
+      tone(BUZZER_PIN, 1000); 
+      delay(200);
+      noTone(BUZZER_PIN);
+      if (i < 2) {
+        delay(200);
       }
     }
   } else {
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(FAN_PIN, HIGH); // Corrected logic to turn off the fan
-    noTone(BUZZER_PIN); // Stop the buzzer
-    forceChange=0;
+    digitalWrite(FAN_PIN, HIGH);
+    noTone(BUZZER_PIN);
+    forceChange = 0;
   }
 }
 
+// Função para imprimir informações de depuração no Serial
 void printDebug(int temperature, int temperatureThreshold, float gasSensorValue, int gasThreshold) {
   Serial.print("--------------------\n");
-  Serial.print("Temperature: ");
+  Serial.print("Temperatura: ");
   Serial.print(temperature);
-  Serial.print(", Temperature Threshold: ");
+  Serial.print(", Limiar de Temperatura: ");
   Serial.print(temperatureThreshold);
-  Serial.print(", Gas: ");
+  Serial.print(", Gás: ");
   Serial.print(gasSensorValue);
-  Serial.print(", Gas Threshold: ");
+  Serial.print(", Limiar de Gás: ");
   Serial.println(gasThreshold);
 }
